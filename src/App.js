75716af68240210417e5031034e99f4592d3d29c
Muradil <04072002mu@gmail.com>
2021-01-09 03:05:27 +0600

    #signin and signUp realize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import {Switch, Route} from "react-router";
+import {connect} from 'react-redux';
+import Home from './components/pages/Home/Home';
+import SignIn from './components/auth/signIn/signIn';
+import SignUp from "./components/auth/signUp/signUp";
+import {auth} from "./config/firebaseConfig";
+import {clearCurrentUser, setCurrentUser} from "./redux/auth/authAction";
 
-function App() {
+
+
+function App({currentUser, clearCurrentUser, setCurrentUser }) {
+    useEffect(()=>{
+        let unsubscribeFromAuth = null;
+
+        unsubscribeFromAuth = auth.onAuthStateChanged(user=>{
+            if (user){
+                setCurrentUser(user)
+            }else {
+                clearCurrentUser()
+            }
+        });
+
+        return () => unsubscribeFromAuth();
+    }, [currentUser, clearCurrentUser, setCurrentUser ]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/sign-in' component={SignIn}/>
+        <Route exact path='/sign-up' component={SignUp}/>
+      </Switch>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state =>({
+    currentUser: state.auth.currentUser
+});
+
+const mapDispatchToProps = dispatch =>({
+    setCurrentUser: user => dispatch(setCurrentUser(user)),
+    clearCurrentUser: () => dispatch(clearCurrentUser())
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
