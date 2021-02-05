eb29d21eed28006140bdddda64d42e00dfc792f6
Muradil <04072002mu@gmail.com>
2021-02-06 02:19:52 +0600

    #fixed css file 20%

--- src/components/UI/sidebar/sidebar.js
+++ src/components/UI/sidebar/sidebar.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import {connect} from  'react-redux';
+import { Image } from 'react-bootstrap';
 
-import s from "../../pages/Home/home.module.css";
+import './sidebar.css';
 import logo from "../../../assets/better-logo.svg";
 import roofIcon from "../../../assets/roof-icon.png";
 
 
 const Sidebar = ({location, city}) => {
     return (
-        <div className={s.side_bar}>
-            <div className={s.logo}>
-                <img src={logo} alt="logo"/>
-                <div className={s.item}>
-                    <img src={roofIcon} alt="ava"/>
-                    {location ?
-                        <div className={s.address}>
-                            <p>{city}</p>
-                            <span>{location}</span>
-                        </div>:
-                        <p>New refinance</p>
-                    }
+        <div id="viewport">
+            <div id="sidebar">
+                <header>
+                    <img src={logo} alt="logo"/>
+                </header>
+                <div className="content">
+                    <div className="item">
+                        <Image src={roofIcon} roundedCircle />
+                        {location ?
+                            <div className="address">
+                                <p>{city}</p>
+                                <span>{location}</span>
+                            </div> :
+                            <p>New refinance</p>
+                        }
+                    </div>
                 </div>
             </div>
         </div>
